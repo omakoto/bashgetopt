7986038fc47f42b79982e28c9f22344e3657ec1c
Makoto Onuki <makoto.onuki@google.com>
2018-06-16 22:49:48 -0700

    update presubmit

--- scripts/presubmit.sh
+++ scripts/presubmit.sh
@@ -11,6 +11,8 @@ gofmt -s -d $(find . -type f -name '*.go') |& perl -pe 'END{exit($. > 0 ? 1 : 0)
 
 go test -v -race ./...                   # Run all the tests with the race detector enabled
 
+./tests/all.bash
+
 echo "Running extra checks..."
 go vet ./...                             # go vet is the official Go static analyzer
 megacheck ./...                          # "go vet on steroids" + linter
